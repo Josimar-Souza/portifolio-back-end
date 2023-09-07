cb4a371311bfe60cdc04cc0670ec350cdc078bf5
Josimar Souza <contact.josimarsouza@gmail.com>
2023-09-07 17:44:25 -0300

    feat: Error middleware created

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const rootRouter = require('../Controllers/rootRouter');
+const ErrorMiddleware = require('../middleware/ErrorMiddleware');
 
 const app = express();
 
 app.use(express.json());
 
 rootRouter(app);
+
+app.use(ErrorMiddleware);
 
 module.exports = app;
