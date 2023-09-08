581f735012597b1cb6b7956cfdc36afe289a5796
Josimar Souza <contact.josimarsouza@gmail.com>
2023-09-08 15:24:30 -0300

    feat: Added GetAllProjects model created

--- src/Model/projects/index.js
+++ src/Model/projects/index.js
@@ -1,7 +1,9 @@
 const AddProject = require('./addProject');
+const GetAllProjects = require('./getAllProjects');
 
 const projectModels = {
   AddProject,
+  GetAllProjects,
 };
 
 module.exports = projectModels;
