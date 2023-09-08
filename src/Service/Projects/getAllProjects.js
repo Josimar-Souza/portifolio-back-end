4f43b33314115741f6bc6630cfff458e89234b92
Josimar Souza <contact.josimarsouza@gmail.com>
2023-09-08 15:32:24 -0300

    feat: GetAllProjects service created

---
+++ src/Service/Projects/getAllProjects.js
@@ -0,0 +1,9 @@
+const projectsModels = require('../../Model/projects');
+
+const GetAllProjects = async () => {
+  const projects = await projectsModels.GetAllProjects();
+
+  return projects;
+};
+
+module.exports = GetAllProjects();
